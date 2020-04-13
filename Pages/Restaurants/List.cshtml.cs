02602c14015a0c1c8db7259d9d798843086aa637
Shola <ayenisholah@yahoo.com>
2020-04-13 22:17:43 +0100

    Building a page model

--- Pages/Restaurants/List.cshtml.cs
+++ Pages/Restaurants/List.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Restaurant_App.Core;
 using Restaurant_App.Data;
 
 namespace Restaurant_App.Pages.Restaurants
@@ -12,14 +13,16 @@ namespace Restaurant_App.Pages.Restaurants
     {
         private readonly IRestaurantData restaurantData;
 
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
         public ListModel(IRestaurantData restaurantData)
         {
             this.restaurantData = restaurantData;
         }
 
-
         public void OnGet()
         {
+            Restaurants = restaurantData.GetAll();
         }
     }
 }
